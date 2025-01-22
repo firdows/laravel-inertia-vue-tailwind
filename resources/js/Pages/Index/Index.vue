60a16e149331dee4bc3b3de6b60f233dc5ea008d
SSUPGROUP\00C226 <ahamad.jedu@gmail.com>
2025-01-22 15:01:52 +0700

    27. Persistent layouts with Inertia

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,16 +1,21 @@
 <template lang="">
-    <MainLayout >
         <div>
             Message : {{message}}
         </div>
-    </MainLayout>
 </template>
+
 <script setup>
 import { ref } from "vue";
-import MainLayout from "../Layouts/MainLayout.vue";
 defineProps({
   message: {
     type: String,
   },
 });
+</script>
+
+<script>
+import MainLayout from "../Layouts/MainLayout.vue";
+export default {
+  layout: MainLayout,
+};
 </script>
