808cf87f394e4c4e6411f2e57f15e26f6f9918ad
Ake.A <ahamad.jehduaramae@ssup.co.th>
2025-01-22 22:02:08 +0700

    47. Computed Data and Conditional Rendering (Displaying Flash Messages)

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,10 +3,14 @@
     <Link href="/hello">Show Page</Link>&nbsp;
     <Link href="/listing">Listing Page</Link>&nbsp;
 
-    <div v-if="$page.props.flash.success" class="alert">
-        {{ $page.props.flash.success }}
+    <div v-if="flashSuccess" class="success">
+        <!-- {{ $page.props.flash.success }} -->
+        {{ flashSuccess }}
       </div>
 
+    <div>
+        x*2 : {{y}}
+    </div>
     <div>
         The page with time : {{timer}}
     </div>
@@ -16,12 +20,24 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { Link } from "@inertiajs/vue3";
-
+import { ref, computed } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
 
 const timer = ref(0);
 setInterval(() => {
-  timer.value++;
+    timer.value++;
 }, 1000);
+
+const x = ref(0);
+const y = computed(() => x.value * 2);
+const page = usePage();
+const flashSuccess = computed(() => page.props.flash.success);
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    padding: 10px;
+    color: #fff;
+}
+</style>
