305676b66bd0cedbdfd799c78d7c3bdca34555f5
Ake.A <ahamad.jehduaramae@ssup.co.th>
2025-01-23 19:51:02 +0700

    61. Extracting Tailwind Styles Using @apply Directive

--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -3,48 +3,51 @@
         <h1>Update</h1>
     </div>
     <form @submit.prevent="create">
-        <div>
-            <label for="">Beds : </label> 
-            <input type="text" v-model.number="form.beds"/>
-            <span class="error" v-show="form.errors.beds">{{form.errors.beds}}</span>
-        </div>
-        <div>
-            <label for="">Baths : </label> 
-            <input type="text" v-model="form.baths"/>
-            <span class="error" v-show="form.errors.baths">{{form.errors.baths}}</span>
-        </div>
-        <div>
-            <label for="">Area : </label> 
-            <input type="text" v-model="form.area"/>
-            <span class="error" v-show="form.errors.baths">{{form.errors.area}}</span>
-        </div>
-        <div>
-            <label for="">City : </label> 
-            <input type="text" v-model="form.city"/>
-            <span class="error" v-show="form.errors.city">{{form.errors.city}}</span>
-        </div>
-        <div>
-            <label for="">Code : </label> 
-            <input type="text" v-model="form.code"/>
-            <span class="error" v-show="form.errors.code">{{form.errors.code}}</span>
-        </div>
-        <div>
-            <label for="">Street : </label> 
-            <input type="text" v-model="form.street"/>
-            <span class="error" v-show="form.errors.street">{{form.errors.street}}</span>
-        </div>
-        <div>
-            <label for="">Street Nr : </label> 
-            <input type="text" v-model="form.street_nr"/>
-            <span class="error" v-show="form.errors.street_nr">{{form.errors.street_nr}}</span>
-        </div>
-        <div>
-            <label for="">Price : </label> 
-            <input type="text" v-model="form.price"/>
-            <span class="error" v-show="form.errors.price">{{form.errors.price}}</span>
-        </div>
-        <div>
-            <button type="submit">Submit</button>
+        
+        <div class="grid grid-cols-6 gap-4">
+            <div class="col-span-2">
+                <label for="" class="block mb-1 text-gray-500 font-medium">Beds : </label> 
+                <input type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" v-model.number="form.beds" />
+                <div class="text-red-600" v-show="true || form.errors.beds">{{form.errors.beds}}555</div>
+            </div>
+            <div class="col-span-2">
+                <label for="" class="block mb-1 text-gray-500 font-medium">Baths : </label> 
+                <input type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" v-model="form.baths"/>
+                <div class="error" v-show="form.errors.baths">{{form.errors.baths}}</div>
+            </div>
+            <div class="col-span-2">
+                <label for="" class="block mb-1 text-gray-500 font-medium">Area : </label> 
+                <input type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" v-model="form.area"/>
+                <div class="error" v-show="form.errors.baths">{{form.errors.area}}</div>
+            </div>
+            <div class="col-span-2">
+                <label for="" class="block mb-1 text-gray-500 font-medium">City : </label> 
+                <input type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" v-model="form.city"/>
+                <div class="error" v-show="form.errors.city">{{form.errors.city}}</div>
+            </div>
+            <div class="col-span-2">
+                <label for="" class="block mb-1 text-gray-500 font-medium">Code : </label> 
+                <input type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" v-model="form.code"/>
+                <div class="error" v-show="form.errors.code">{{form.errors.code}}</div>
+            </div>
+            <div class="col-span-3">
+                <label for="" class="block mb-1 text-gray-500 font-medium">Street : </label> 
+                <input type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" v-model="form.street"/>
+                <div class="error" v-show="form.errors.street">{{form.errors.street}}</div>
+            </div>
+            <div class="col-span-3">
+                <label for="" class="block mb-1 text-gray-500 font-medium">Street Nr : </label> 
+                <input type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" v-model="form.street_nr"/>
+                <div class="error" v-show="form.errors.street_nr">{{form.errors.street_nr}}</div>
+            </div>
+            <div class="col-span-6">
+                <label for="" class="block mb-1 text-gray-500 font-medium">Price : </label> 
+                <input type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600" v-model="form.price"/>
+                <div class="error" v-show="form.errors.price">{{form.errors.price}}</div>
+            </div>
+            <div class="col-span-6">
+                <button type="submit" class="p-2 border-md bg-green-600 rounded-md hover:bg-green-900 text-white">Submit</button>
+            </div>
         </div>
 
        
@@ -68,7 +71,8 @@ const form = useForm({
     street_nr: props.listing.street_nr || 0,
     price: props.listing.price || 0,
 });
-const create = () => form.put(route("listing.update",{listing:props.listing.id}));
+const create = () =>
+    form.put(route("listing.update", { listing: props.listing.id }));
 </script>
 <style scoped>
 label {
@@ -77,8 +81,8 @@ label {
 div {
     padding: 3px;
 }
-span.error{
-    color:red;
+span.error {
+    color: red;
 }
 </style>
 
