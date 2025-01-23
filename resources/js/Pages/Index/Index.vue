7879f1b1aad65e6c8ffce4103bf9fafedd7800d2
SSUPGROUP\00C226 <ahamad.jedu@gmail.com>
2025-01-23 13:10:51 +0700

    54. Why Tailwind CSS? Tailwind Overview

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -2,14 +2,23 @@
         <div>
             Message : {{message}}
         </div>
+
+        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center gap-x-4">
+    <div class="shrink-0">
+        <img class="size-12" src="/img/logo.svg" alt="ChitChat Logo">
+    </div>
+    <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-slate-500">You have a new message!</p>
+    </div>
+    </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 defineProps({
-  message: {
-    type: String,
-  },
+    message: {
+        type: String,
+    },
 });
 </script>
-
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,9 +1,9 @@
 <template lang="">
 
-<p>
-    <Link :href="route('listing.create')" class="border-m bg-green-500 p-3 rounded-md m-3">Create</Link>
+<p class="mt-5">
+    <Link :href="route('listing.create')" class="border-m p-3 rounded-md m-3 bg-sky-500 hover:bg-sky-700 text-white">Create</Link>
 </p>
-
+<div class="mt-5">
   <div v-for="listing in listings" :key="listing.id">
     <Link :href="route('listing.show',{listing:listing.id})">
       <ListingAddress :listing="listing" />
@@ -12,6 +12,7 @@
     <Link :href="route('listing.edit', { listing: listing.id })"> Edit </Link> | 
     <Link :href="route('listing.destroy', {listing: listing.id})" method="DELETE" as="button"> Delete </Link>
   </div>
+</div>
 </template>
 <script setup>
 import { Link } from "@inertiajs/vue3";
