a727ac23eb22604f61b4bb96faf08485e1657712
Ake.A <ahamad.jehduaramae@ssup.co.th>
2025-01-25 14:48:01 +0700

    98. Adding Pagination

---
+++ resources/js/Pages/Listing/Index/Components/Pagination.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <div class="flex gap-1">
+          
+        <Link 
+            v-for="(link,index) in links" :key="index"
+            :href="link.url"  
+            v-html="link.label"
+            class="py-2 px-4 rounded-md"
+            :class="{'bg-indigo-500 dark:bg-indigo-800 text-gray-100':link.active}"
+            >
+        </Link>
+           
+    </div>
+</template>
+
+<script setup>
+import {Link} from "@inertiajs/vue3";
+
+const props = defineProps({
+    links:Array
+})
+
+</script>
