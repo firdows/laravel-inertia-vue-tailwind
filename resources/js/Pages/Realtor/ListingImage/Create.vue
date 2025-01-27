6412b4a134e9a8c4391fdc88a610dffb954e6a8a
SSUPGROUP\00C226 <ahamad.jedu@gmail.com>
2025-01-27 14:39:15 +0700

    125. Uploading Multiple Images (In Vue)

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,23 +1,37 @@
 <template lang="">
-    <div>
-        Create Image
-    </div>
-    <!-- <form @submit.prevent="submit"> -->
-    <form method="POST" :action="route('realtor.listing.image.store', { listing: listing.id })" enctype="multipart/form-data">
-        <input type="file" multiple name='images[]' />
-        <button type="submit" class="btn-primary">Upload</button>
-    </form>
+    <Box>
+        <template #header>Upload Image</template>
+        <form @submit.prevent="submit">
+       
+            <input type="file" multiple @input="addFiles"/>
+            
+            <button type="submit" class="btn-outline">Upload</button>
+            <button type="reset" class="btn-outline" @click="reset">Reset</button>
+        </form>
+    </Box>
 </template>
 <script setup>
-import { useForm } from '@inertiajs/vue3';
+import { useForm } from "@inertiajs/vue3";
+import Box from "@/Components/UI/Box.vue";
 
-defineProps({ listing: Object })
+const props = defineProps({ listing: Object });
 
-// const form = useForm({
-//     file:null
-// })
+const form = useForm({
+    images: []
+});
 
-// const submit = () => {
-//     form.post(route('realtor.listing.image.store', { listing: listing.id }));
-// }
+const submit = () => {
+    form.post(route('realtor.listing.image.store', { listing: props.listing.id }), {
+        onSuccess: () => form.reset('images')
+    });
+}
+
+const addFiles = (e) => {
+    // console.log(e.target.files);
+    for (const image of e.target.files) {
+        // console.log(image);
+        form.images.push(image);
+    }
+}
+const reset = () => form.reset('images')
 </script>
