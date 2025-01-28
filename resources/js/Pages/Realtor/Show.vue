f8037cbaae794fed3c2db98b463cc82364ce314a
SSUPGROUP\00C226 <ahamad.jedu@gmail.com>
2025-01-28 17:13:30 +0700

    141. Single Listing View with All The Offers

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="mt-2">
-    <Link :href="route('realtor.listing.index')"><< Back to listing</Link>
+    <Link :href="route('realtor.listing.index')">← Go back to Listings</Link>
   </div>
 
   <div class="flex mt-2 flex-col-reverse md:grid md:grid-cols-12 gap-4">
-    <Box
-      v-if="hasOffers"
-      class="flex md:col-span-7 items-center "
-    >
-      <div v-for="offer in listing.offers" :key="offer.id">
-        <Price :price="offer.amount" />
-      </div>
+    <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
+      <div class="w-full text-center font-medium text-gray-500">No offers</div>
     </Box>
 
-    <div v-else class="md:col-span-7">
-      This is diplayed when there are offers!
+    <div v-else class="md:col-span-7 items-center">
+      <OfferItem
+        v-for="offer in listing.offers"
+        :key="offer.id"
+        class="mb-4"
+        :offer="offer"
+        :listing-price="listing.price"
+      />
     </div>
 
     <Box class="md:col-span-5 flex flex-col gap-4">
@@ -34,6 +35,7 @@ import Price from "@/Components/Price.vue";
 import Box from "@/Components/UI/Box.vue";
 import { reactive, ref, computed } from "vue";
 import { useMonthlyPayment } from "@/Composables/useMonthlyPayment";
+import OfferItem from "./Show/Components/OfferItem.vue";
 
 const props = defineProps({
   listing: Object,
